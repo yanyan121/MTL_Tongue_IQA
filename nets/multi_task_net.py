588291579742d827ec3da6bd5cacb62b8ee85b0b
yanyan121 <30499312+yanyan121@users.noreply.github.com>
2022-07-10 08:08:55 +0800

    Update multi_task_net.py

--- nets/multi_task_net.py
+++ nets/multi_task_net.py
@@ -59,14 +59,14 @@ class Cla_model_single(nn.Module):
 class multi_model_unet(nn.Module):
     def __init__(self, num_classes=2, pretrained=False, backbone='resnet18', uncertainty='normal', n_samples=5, p=0.0):
         super(multi_model_unet, self).__init__()
-        if backbone == 'vgg':
-            self.vgg = VGG(pretrained=pretrained)
-            in_filters = [192, 384, 768, 1024]
-        elif backbone == "resnet18":
-            self.resnet = resnet18(pretrained=pretrained)
-            in_filters = [192, 512, 1024, 3072]
-        else:
-            raise ValueError('Unsupported backbone - `{}`, Use vgg, resnet18.'.format(backbone))
+        ## shared layers
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 3, kernel_size=3, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpooling = nn.MaxPool2d(2, stride=2)
+       ## segmentation_net
+        self.vgg = VGG(pretrained=pretrained)
+        in_filters = [192, 384, 768, 1024]
         out_filters = [64, 128, 256, 512]
         # upsampling_1
         # 64,64,512
@@ -77,31 +77,11 @@ class multi_model_unet(nn.Module):
         self.up_concat2 = unetUp(in_filters[1], out_filters[1], p=p)
         # 512,512,64
         self.up_concat1 = unetUp(in_filters[0], out_filters[0], p=p)
-        # upsampling_2
-        # 64,64,512
-        self.up_concat4_2 = unetUp(in_filters[3], out_filters[3], p=p)
-        # 128,128,256
-        self.up_concat3_2 = unetUp(in_filters[2], out_filters[2], p=p)
-        # 256,256,128
-        self.up_concat2_2 = unetUp(in_filters[1], out_filters[1], p=p)
-        # 512,512,64
-        self.up_concat1_2 = unetUp(in_filters[0], out_filters[0], p=p)
-        if backbone == 'resnet18':
-            self.up_conv = nn.Sequential(
-                nn.UpsamplingBilinear2d(scale_factor=2),
-                nn.Conv2d(out_filters[0], out_filters[0], kernel_size=3, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(out_filters[0], out_filters[0], kernel_size=3, padding=1),
-                nn.ReLU(),
-            )
-        else:
-            self.up_conv = None
+
+        self.up_conv = None
 
         self.final = nn.Conv2d(out_filters[0], num_classes, 1)
 
-        self.uncertainty = uncertainty
-        self.n_samples = n_samples
-        self.backbone = backbone
 
         ## classification
         self.cla_net = Cla_model_single()
@@ -110,19 +90,18 @@ class multi_model_unet(nn.Module):
         # self.model.conv1 = nn.Conv2d(9, 64, kernel_size=(7, 7), stride=(1, 1), padding=(3, 3), bias=False)
         self.model.fc = nn.Linear(512, 2)
 
-        # self.sa_seg = SelfAttention(1024, 'relu')
-        # self.sa_cla = SelfAttention(1024, 'relu')
-        # self.concat = torch.cat()
-        self.cat_conv_seg = nn.Conv2d(1024, 512, 3, 1, 1)
-        self.cat_conv_cla = nn.Conv2d(1024, 512, 3, 1, 1)
+
 
     def forward(self, inputs):  # 嵌套整个网络模型函数，方便调用
-        if self.backbone == "vgg":
-            [feat1, feat2, feat3, feat4, feat5] = self.vgg.forward(inputs)
-        elif self.backbone == "resnet18":
-            [feat1, feat2, feat3, feat4, feat5] = self.resnet.forward(inputs)
+        # shared_lay
+        shared_feat = self.conv1(inputs)
+        shared_feat = self.relu(shared_feat)
+        shared_feat = self.conv2(shared_feat)
+        shared_feat = self.relu(shared_feat)
+        shared_feat = self.maxpooling(shared_feat)
+        [feat1, feat2, feat3, feat4, feat5] = self.vgg.forward(shared_feat)
 
-        results_cla = self.cla_net.forward(inputs)
+        results_cla = self.cla_net.forward(shared_feat)
         convlayer4_cla = results_cla['convlayer4']
         share_cat = torch.cat((feat5, convlayer4_cla), 0)
         ## cla
